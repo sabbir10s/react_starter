44a49d996cdc762921948fefb92338b897615aa5
sabbir10s <sabbirahmed1023@gmail.com>
2023-11-29 17:13:34 +0600

    react starte created

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-xl text-center"> This is Font family</h2>
+      <h3 className="text-xl text-center font-poppins font-bold">
+        This is Font family
+      </h3>
+    </div>
+  );
+};
+
+export default Home;
